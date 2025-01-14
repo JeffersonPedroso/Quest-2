7998b899eb7583084a7f3701a87bba3699ee7ade
jefferson <jefferson182@icloud.com>
2025-01-14 19:44:25 -0300

    finalizado

---
+++ form.js
@@ -0,0 +1,21 @@
+
+const btnEnviar = document.getElementById('btn-enviar');
+const formulario = document.getElementById("form");
+
+btnEnviar.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const inputs = formulario.querySelectorAll("input, textarea");
+    inputs.forEach((campo) => {
+        if ((campo.value || "").trim() === "") {
+            campo.nextElementSibling?.classList.remove("oculto");
+            campo.classList.add("campo-obrigatorio");
+            
+        } else {
+            campo.classList.remove("campo-obrigatorio");
+            campo.nextElementSibling?.classList.add("oculto");
+            campo.classList.add("campo-preenchido");
+        }
+    });
+});
+
